0387e5ad69937ec71c1d56d88b1d448c22535127
da9630jj <da9630jj@gmail.com>
2024-09-11 15:33:59 +0900

    dijhf

--- src/main/java/com/green/team3_app/rec/controller/RecController.java
+++ src/main/java/com/green/team3_app/rec/controller/RecController.java
@@ -1,7 +1,14 @@
 package com.green.team3_app.rec.controller;
 
+import com.green.team3_app.rec.service.RecService;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/rec")
 public class RecController {
+    @Resource(name = "recService")
+    private RecService recService;
 }
